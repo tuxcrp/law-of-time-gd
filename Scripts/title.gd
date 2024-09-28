028d72ce614a4de8cd8bb86aa812871d22c51291
Joseph Chacko <josephchacko2006@gmail.com>
2024-09-28 23:41:19 +0530

    Initial commit

---
+++ Scripts/title.gd
@@ -0,0 +1,15 @@
+extends Node2D
+@onready var node_2d = $Fade/AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_released("LMB") or Input.is_action_just_released("RMB"):
+		node_2d.play("fade_in")
+		await get_tree().create_timer(0.5).timeout
+		get_tree().change_scene_to_file("res://level-1.tscn")
