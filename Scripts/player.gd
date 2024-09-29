d74ff39a85ffbf0e8c5f10d7afb7fe0333eabb23
zoclhas <zoclhas@gmail.com>
2024-09-29 18:16:17 +0530

    Merged both player scripts into one

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
 var speed = 1000
-@export var target_position:Vector2 
+@export var target_position: Vector2 
 
-@export var grid_size:int = 32
+@export var grid_size: int = 32
 
-@export var left:String = "left"
-@export var right:String = "right"
-@export var up:String = "up"
-@export var down:String = "down"
-@export var other:CharacterBody2D
+@export var left: String = "left"
+@export var right: String = "right"
+@export var up: String = "up"
+@export var down: String = "down"
+@export var other: CharacterBody2D
+@export var is_player_a: bool = true
 
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var eyes: Sprite2D = $eyes
@@ -27,20 +28,24 @@ func snap_to_grid(pos) -> Vector2:
 	var snapped_x = round(pos.x / grid_size) * grid_size
 	var snapped_y = round(pos.y / grid_size) * grid_size
 	return Vector2(snapped_x, snapped_y)
-	
-	
+
 func _process(delta: float) -> void:
 	var target = other.position - position
 	eyes.position = target.normalized() * 5
-	
-			
+
 func _physics_process(delta):
 	if (target_position - position).length() > 10:
-		Globals.time_paused_a = false
+		if is_player_a:
+			Globals.time_paused_a = false
+		else:
+			Globals.time_paused_b = false
 		var direction = (target_position - position).normalized()
 		position += direction * speed * delta
 	else:
-		Globals.time_paused_a = true
+		if is_player_a:
+			Globals.time_paused_a = true
+		else:
+			Globals.time_paused_b = true
 		position = target_position
 		var movement_vector = Vector2.ZERO
 		if Input.is_action_just_pressed(left) and not is_wall(Vector2i(-1, 0), position):
@@ -57,4 +62,3 @@ func _physics_process(delta):
 		if movement_vector != Vector2.ZERO:
 			target_position = snap_to_grid(position + movement_vector * grid_size)
 			audio_stream_player_2d.play()
-		
