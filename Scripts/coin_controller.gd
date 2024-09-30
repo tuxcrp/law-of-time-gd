d384282b05ac009a69e3ef59f47b91aab5613195
zoclhas <zoclhas@gmail.com>
2024-09-30 23:47:50 +0530

    Coin sound?

--- Scripts/coin_controller.gd
+++ Scripts/coin_controller.gd
@@ -3,7 +3,10 @@ extends AnimatedSprite2D
 
 @export var is_white: bool
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	audio_stream_player_2d.play()
 	if is_white:
 		if body.is_player_a:
 			Globals.white_coins += 1
